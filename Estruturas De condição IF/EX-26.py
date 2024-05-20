3f1084163744a042a53ec96dcedd05bd232008d2
Diego Honório <diegohonoriiio@gmail.com>
2024-05-20 18:19:41 -0300

    Create EX-26.py
    novo exercicio

---
+++ Estruturas De condição IF/EX-26.py
@@ -0,0 +1,45 @@
+# Cabeçalho do sistema 
+print('=' * 60)
+print('SISTEMA DE DESCONTO DE COMBUSTIVEL'.center(60))
+print('=' * 60)
+
+# Preço de combustiveis
+LitroAlcool = 1.90
+LitroGasolina = 2.50
+
+
+Litros = float(input('Informe a quantia de litros: '))
+print('-' * 60)
+print('[1] - ALCOOL')
+print('[2] - GASOLINA')
+print('-' * 60)
+Escolha = int(input('Escolha: '))
+print('-' * 60)
+
+if Escolha == 1:
+    if Litros <= 20:
+        ValorLitros = (Litros * LitroAlcool)
+        Desconto = ValorLitros * 0.03
+        Total = ValorLitros - Desconto
+
+    elif Litros > 20:
+        ValorLitros = (Litros * LitroAlcool)
+        Desconto = ValorLitros * 0.05
+        Total = ValorLitros - Desconto
+    else:
+        pass
+
+elif Escolha == 2:
+    if Litros <= 20:
+        ValorLitros = (Litros * LitroGasolina)
+        Desconto = ValorLitros * 0.04
+        Total = ValorLitros - Desconto
+    elif Litros > 20:
+        ValorLitros = (Litros * LitroGasolina)
+        Desconto = ValorLitros * 0.06
+        Total = ValorLitros - Desconto
+    else:
+        pass
+else:
+    print('Informe um valor valido.')
+
