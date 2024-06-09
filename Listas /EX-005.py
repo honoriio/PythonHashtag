1606ebbe26a95a0814a4374ea56c2a280da78c6f
Diego Honório <diegohonoriiio@gmail.com>
2024-06-09 18:37:46 -0300

    Correção
    Adicionado um if para o caso de não houver um item cahamado livro o programa possa funcionar corretamente

--- Listas /EX-005.py
+++ Listas /EX-005.py
@@ -13,49 +13,53 @@ produtos_ecommerce = [
     [1900, 400]
 ]
 
-#Achando o indice do produto livro, valores e quantias vendidas 
-IndiceProduto = produtos.index('livro')
-IndicePreco = produtos_ecommerce[IndiceProduto]
-Quantia = IndicePreco[0]
-valor = IndicePreco[1]
+
+if 'livro' in produtos:
+    #Achando o indice do produto livro, valores e quantias vendidas 
+    IndiceProduto = produtos.index('livro') 
+    IndicePreco = produtos_ecommerce[IndiceProduto]
+    Quantia = IndicePreco[0]
+    valor = IndicePreco[1]
 
 
 
-# Calculo dos juros encima do valor dos livros.
-Juros = (valor * 10) / 100
+    # Calculo dos juros encima do valor dos livros.
+    Juros = (valor * 10) / 100
 
-# Atribuindo o valor do juros ao produto
+    # Atribuindo o valor do juros ao produto
 
-ValorComJuros = (valor + Juros)
+    ValorComJuros = (valor + Juros)
 
-#Aualizando o valor dos juros ao item 
+    #Aualizando o valor dos juros ao item 
 
-produtos_ecommerce.pop(IndiceProduto)
-NovoPreco = [Quantia, ValorComJuros]
-produtos_ecommerce[IndiceProduto] = produtos_ecommerce[IndiceProduto] = NovoPreco
+    produtos_ecommerce.pop(IndiceProduto)   
+    NovoPreco = [Quantia, ValorComJuros]
+    produtos_ecommerce[IndiceProduto] = produtos_ecommerce[IndiceProduto] = NovoPreco
 
-CustoAntigo = (Quantia * valor)
-NovoCusto = (Quantia * ValorComJuros)
-Aumento = (NovoCusto - CustoAntigo)
+    CustoAntigo = (Quantia * valor)
+    NovoCusto = (Quantia * ValorComJuros)
+    Aumento = (NovoCusto - CustoAntigo)
 
-print('=' * 120)
-print(f'Tabela atualizada'.center(120))
-print('=' * 120)
-print(produtos)
-print('-' * 120)
-print(produtos_ecommerce)
+    print('=' * 120)
+    print(f'Tabela atualizada'.center(120))
+    print('=' * 120)
+    print(produtos)
+    print('-' * 120)
+    print(produtos_ecommerce)
 
-print('=' * 120)
-print(f'Com o novo imposto o novo custo sera de R${ValorComJuros:.2f}')
-print('-' * 120)
-print(f'O custo antigo era de R${ValorComJuros:.2f}')
-print('=' * 120)
-print('Sendo assim, o custo total sera de')
-print(f'R${NovoCusto:,.2f}')
-print('-' * 120)
-print('O custo anterior era de')
-print(f'R${CustoAntigo:,.2f}')
-print('-' * 120)
-print('Sendo assim ouve um aumento de')
-print(f'R${Aumento:,.2f}')
-print('=' * 120)
+    print('=' * 120)
+    print(f'Com o novo imposto o novo custo sera de R${ValorComJuros:.2f}')
+    print('-' * 120)
+    print(f'O custo antigo era de R${ValorComJuros:.2f}')
+    print('=' * 120)
+    print('Sendo assim, o custo total sera de')
+    print(f'R${NovoCusto:,.2f}')
+    print('-' * 120)
+    print('O custo anterior era de')
+    print(f'R${CustoAntigo:,.2f}')
+    print('-' * 120)
+    print('Sendo assim ouve um aumento de')
+    print(f'R${Aumento:,.2f}')
+    print('=' * 120)
+else:
+    pass
