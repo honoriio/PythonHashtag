709e51daf301ee0325e47f8bde062a68634b732a
Diego Honório <diegohonoriiio@gmail.com>
2024-06-21 20:00:38 -0300

    Novo exercicio criado
    Adicionafo um novo exercicio usando laço while

---
+++ Estruturas de repetição While/EX-001.py
@@ -0,0 +1,12 @@
+vendas = [941, 852, 783, 714, 697, 686, 685, 670, 631, 453, 386, 371, 294, 269, 259, 218, 208, 163, 125, 102, 87, 47, 7]
+vendedores = ['Maria', 'José', 'Antônio', 'João', 'Francisco', 'Ana', 'Luiz', 'Paulo', 'Carlos', 'Manoel', 'Pedro',
+             'Francisca', 'Marcos', 'Raimundo', 'Sebastião', 'Antônia', 'Marcelo', 'Jorge', 'Márcia', 'Geraldo', 'Adriana', 'Sandra', 'Luis']
+meta = 50
+
+i = 0
+
+while vendas[i] > meta:
+    i += 1
+    print('-' * 60)
+    print(f'{vendedores[i]} Bateu a meta de vendas com {vendas[i]} em vendas.')
+
