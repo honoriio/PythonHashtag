eb1857556e764882bac4ecbd04e6ea1767f5e4b1
Diego Honório <diegohonoriiio@gmail.com>
2024-07-15 11:51:02 -0300

    aaa

---
+++ function/EX-001.py
@@ -0,0 +1,9 @@
+
+from PythonHashtag.function.funcao import soma
+numero1 = float(input('Numero 1 : '))
+numero2 = float(input('Numero 2 : '))
+numero3 = float(input('Numero 3 : '))
+
+soma = soma(numero1, numero2, numero3)
+
+print(soma)
---
+++ function/funcao.py
@@ -0,0 +1,2 @@
+def soma(numero1, numero2, numero3):
+    return (numero1 + numero2) / numero3
